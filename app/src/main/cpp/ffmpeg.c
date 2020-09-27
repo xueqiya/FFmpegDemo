a85bf56221e1446ff611072bedc835f0321cc761
xueqi <xueqi@zjmedia.net>
2020-09-27 14:43:28 +0800

    fix: 进城结束后exit_program

--- app/src/main/cpp/ffmpeg.c
+++ app/src/main/cpp/ffmpeg.c
@@ -4919,6 +4919,7 @@ int run(int argc, char **argv)
 //        exit_program(69);
 
 //    exit_program(received_nb_signals ? 255 : main_return_code);
+    ffmpeg_exited = 1;
     nb_filtergraphs = 0;
     progress_avio = NULL;
     input_streams = NULL;
@@ -4929,6 +4930,11 @@ int run(int argc, char **argv)
     nb_output_streams = 0;
     output_files = NULL;
     nb_output_files = 0;
+    run_as_daemon = 0;
+    nb_frames_dup = 0;
+    dup_warning = 1000;
+    nb_frames_drop = 0;
+    want_sdp = 1;
     return main_return_code;
 }
 
